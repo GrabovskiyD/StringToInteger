542ff272ceef02ff393760368d2c148e0fff97f4
Дмитрий Грабовский <dimokurl@gmail.com>
2023-01-11 19:09:03 +0300

    Добавьте файлы проекта.

---
+++ StringToInteger/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Text.RegularExpressions;
+
+namespace StringToInteger
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            string[] strings = { "+-12" };
+            foreach(string s in strings)
+            {
+                Console.WriteLine(MyAtoi(s));
+            }
+        }
+
+        public static int MyAtoi(string s)
+        {
+            Regex regex = new(@"^(\s*)*(\+{0,1}\-{0,1}\d+)");
+            MatchCollection matches = regex.Matches(s);
+            bool condition = matches.Count > 0 ? 
+                matches[0].Groups[2].ToString().Contains('-') && !matches[0].Groups[2].ToString().Contains('+') ||
+                !matches[0].Groups[2].ToString().Contains('-') && matches[0].Groups[2].ToString().Contains('+') ||
+                !matches[0].Groups[2].ToString().Contains('-') && !matches[0].Groups[2].ToString().Contains('+')  ?
+                true :
+                false : 
+                false;
+            if(condition)
+            {
+                string match = matches[0].Groups[2].ToString();
+                bool isSuccess = int.TryParse(match, out int result);
+                if (isSuccess)
+                {
+                    return result;
+                }
+                else
+                {
+                    if (match.Contains('-'))
+                    {
+                        return int.MinValue;
+                    }
+                    else
+                    {
+                        return int.MaxValue;
+                    }
+                }
+            }
+            else
+            {
+                return 0;
+            }
+        }
+    }
+}
